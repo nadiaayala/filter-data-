7c6470f9bbd6a0d449f9da6e06c796537126ae90
Nadia <nadiaayalanogueira@outlook.com>
2018-12-22 22:11:01 +0400

    User input for values

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,58 @@
-$(function(){
-var people = [
-    {
-        name: 'Ana',
-        rate: '70'
-    },
-    {
-        name: 'John',
-        rate: '65'
-    },
-    {
-        name: 'Mark',
-        rate: '120'
-    },
-    {
-        name: 'Lisa',
-        rate: '85'
-    }
-]
+$(function () {
+    var people = [
+        {
+            name: 'Ana',
+            rate: '70'
+        },
+        {
+            name: 'John',
+            rate: '65'
+        },
+        {
+            name: 'Mark',
+            rate: '120'
+        },
+        {
+            name: 'Lisa',
+            rate: '85'
+        }
+    ]
 
-var results = [];
-people.forEach(function(person){
-    if(person.rate >=60 && person.rate <= 90){
-        results.push(person);
-        console.log(results);              
-    }    
-});
-var $tableHead = $('<thead></thead>');
-$tableHeadTr = $('<tr></tr>');
-var $tableHeadName = $('<td></td>');
-$tableHeadName.text('Name');
-$tableHeadRate = $('<td></td>');
-$tableHeadRate.text('Rate');
-$tableHeadTr.append($tableHeadName);
-$tableHeadTr.append($tableHeadRate);
-$tableHead.append($tableHeadTr);
-var $table = $('<tbody></tbody>');
+    $('#btn').on('click', function () {
+        var $minVal = $('#min').val();
+        var $maxVal = $('#max').val();
+        var results = [];
+        people.forEach(function (person) {
+            if (person.rate >= $minVal && person.rate <= $maxVal) {
+                results.push(person);
+                console.log(results);
+            }
+        });
+        var $tableFull = $('<table></table>');
+        var $tableHead = $('<thead></thead>');
+        var $tableHeadTr = $('<tr></tr>');
+        var $tableHeadName = $('<td></td>');
+        $tableHeadName.text('Name');
+        $tableHeadRate = $('<td></td>');
+        $tableHeadRate.text('Rate');
+        $tableHeadTr.append($tableHeadName);
+        $tableHeadTr.append($tableHeadRate);
+        $tableHead.append($tableHeadTr);
+        var $table = $('<tbody></tbody>');
 
-for(var i = 0; i<results.length; i++){
-    var person = results[i];
-    var $row = $('<tr></tr>');
-    var $tdName = $('<td></td>');
-    var $tdRate = $('<td></td>');
-    $tdName.text(person.name);
-    $tdRate.text(person.rate);
-    $row.append($tdName);
-    $row.append($tdRate);
-    $table.append($row);
-}
-$('body').append($tableHead);
-$('body').append($table);
+        for (var i = 0; i < results.length; i++) {
+            var person = results[i];
+            var $row = $('<tr></tr>');
+            var $tdName = $('<td></td>');
+            var $tdRate = $('<td></td>');
+            $tdName.text(person.name);
+            $tdRate.text(person.rate);
+            $row.append($tdName);
+            $row.append($tdRate);
+            $table.append($row);
+        }
+        $($tableFull).append($tableHead);
+        $($tableFull).append($table);
+        $('body').append($tableFull);
+    })
 })
